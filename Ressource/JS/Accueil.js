a256fe40ab3fe6fa654d4919ec45e69f7ee53672
Fram <framhawk@protonmail.com>
2023-12-07 21:01:07 +0100

    PageGarde
    Design casi finie

--- Ressource/JS/Accueil.js
+++ Ressource/JS/Accueil.js
@@ -1,17 +1,22 @@
 // Navigation
 let pageIndice = 0;
-let pageMax = 3;
+let pageMax = 2;
 
 let flecheDroite = document.querySelector('#fleche-droite');
 let flecheGauche = document.querySelector('#fleche-gauche');
+let partieIndicateur = document.querySelectorAll('.partie-container p');
+let partieDiv = document.querySelectorAll('.container');
 
 if (pageIndice == 0) {
     flecheGauche.style.display = 'none';
 }
 
+updateTitle();
+updateDiv();
+
 function checkIndice(mouvement){
     pageIndice += mouvement;
-    if (pageIndice == 3) {
+    if (pageIndice == 2) {
         flecheDroite.style.display = 'none';
     }else{
         flecheDroite.style.display = 'block';
@@ -25,10 +30,29 @@ function checkIndice(mouvement){
     console.log(pageIndice);
 }
 
+function updateTitle(){
+    partieIndicateur.forEach(element =>
+        element.style.color = "var(--vertGris)"
+    );
+    partieIndicateur[pageIndice].style.color="#fff";
+}
+
+function updateDiv(){
+    partieDiv.forEach(element => 
+        element.style.display = "none"
+    );
+
+    partieDiv[pageIndice].style.display = "block";
+}
+
 flecheDroite.addEventListener('click',function(event){
     checkIndice(1);
+    updateTitle();
+    updateDiv();
 });
 
 flecheGauche.addEventListener('click',function(event){
     checkIndice(-1);
+    updateTitle()
+    updateDiv();
 });
