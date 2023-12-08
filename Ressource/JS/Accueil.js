cee42d297ec13d4fc574efa58ac21ca3a7785263
Fram <framhawk@protonmail.com>
2023-12-08 04:53:10 +0100

    Test2

--- Ressource/JS/Accueil.js
+++ Ressource/JS/Accueil.js
@@ -67,22 +67,3 @@ titreChangement.forEach((element,count) => {
         updateDiv();
     });
 });
-
-// Changement de theme 
-let fond1 = document.querySelector('#fond1');
-let fond2 = document.querySelector('#fond2');
-
-fond1.addEventListener('click',function(event){
-    document.querySelector('body').style.backgroundImage = "url('../Ressource/Images/fondForet1.jpg')";
-    document.querySelectorAll('.container h2').forEach(element => {
-        element.style.color="var(--spec)";
-    });
-});
-
-fond2.addEventListener('click',function(event){
-    document.querySelector('body').style.backgroundImage = "url('../Ressource/Images/fondMer1.jpg')";
-    document.querySelectorAll('.container h2').forEach(element => {
-        element.style.color="var(--spec)";
-    });
-});
-
